5aba45b6b01643635dc63ae25fca25ee24839788
Ioannis Ioannou <ioannisioannou16@gmail.com>
2020-12-31 07:12:47 +0000

    first commit

---
+++ udagram-api-feed/src/controllers/v0/index.router.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import {FeedRouter} from './feed/routes/feed.router';
+
+const router: Router = Router();
+
+router.use('/feed', FeedRouter);
+
+export const IndexRouter: Router = router;
